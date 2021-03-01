67c7a808f8656d3411ec8af3d8853ecc6d87c2aa
Gipos13 <giparakis13@hotmail.com>
2021-03-01 16:32:22 +0200

    we have our first testgit add .

--- back-end/src/test/java/ntua/softeng28/evcharge/UnitTests/OperatorSpec.groovy
+++ back-end/src/test/java/ntua/softeng28/evcharge/UnitTests/OperatorSpec.groovy
@@ -2,27 +2,31 @@ package ntua.softeng28.evcharge.UnitTests
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 import ntua.softeng28.evcharge.operator.*
 import spock.lang.Specification
 
 @DataJpaTest
-@ActiveProfiles("Test")
+@TestPropertySource(locations="classpath:application-test.properties")
 class OperatorSpec extends Specification {
 	@Autowired
 	private OperatorRepository operatorrepo
 	
-	def operator = new Operator(1L,"John Cena")
+	def operator1 = new Operator(1L,"John Cena")
+	def operator2 = new Operator(2L,"Cristiano Ronaldo")
+	def operator3 = new Operator(3L,"Kobe Bryant")
 	
 	def "Check if crud is ok"(){
 		given:
-		def savedoperator=operatorrepo.save(operator)
+		operatorrepo.save(operator1)
+		operatorrepo.save(operator2)
+		operatorrepo.save(operator3)
 		
 		when:
-		def operatorfromdb=operatorrepo.findById(operator.getId())
+		def List<Operator> operatorsfromdb=operatorrepo.findAll()
 		
 		then:
-		operatorfromdb.getId()==savedoperator.getId()
+		operatorsfromdb.size == 3
 	}
 }
