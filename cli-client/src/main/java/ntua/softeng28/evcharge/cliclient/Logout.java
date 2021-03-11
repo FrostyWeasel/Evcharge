c9204391b9af550c72bf72d2db9ab8ef4cfd2b7b
Constantinos Pefkianakis <cpefkianakis@gmail.com>
2021-03-11 20:46:53 +0200

    Added logout command

---
+++ cli-client/src/main/java/ntua/softeng28/evcharge/cliclient/Logout.java
@@ -0,0 +1,69 @@
+package ntua.softeng28.evcharge.cliclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.json.*;
+import org.json.*;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.*;
+import java.lang.*;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+import java.util.Arrays;
+import java.util.List;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import javax.net.ssl.HttpsURLConnection;
+
+@Command(name = "logout", description = "User login")
+public class Logout implements Callable<Integer> {
+
+    String token;
+    String username;
+    File login_token = new File("softeng20bAPI.token");
+
+    @Override
+    public Integer call() throws IOException {
+
+        if(login_token.exists()){
+            System.out.println("Goodbye " + this.getUserName(login_token) + "\nLogout Successful!");
+            login_token.delete();
+        }
+        else
+            System.out.println("Please login first!");
+
+        return 0;
+    }
+
+    public String getToken(File file) throws IOException{
+        String line;
+        String delims = "[:]";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        while((line = reader.readLine()) != null){
+            String[] tokens = line.split(delims);
+            token = tokens[1];
+        }
+        return token;
+    }
+
+    public String getUserName(File file) throws IOException{
+        String line;
+        String delims = "[:]";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        while((line = reader.readLine()) != null){
+            String[] tokens = line.split(delims);
+            username = tokens[0];
+        }
+        return username;
+    }
+
+    public static void main(String[] args) {
+    }
+}
