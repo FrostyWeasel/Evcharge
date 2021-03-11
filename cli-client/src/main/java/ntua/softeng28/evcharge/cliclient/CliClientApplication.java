75b5ca2ca70f46143423caef63c3fcc9ddfbe0f3
Constantinos Pefkianakis <cpefkianakis@gmail.com>
2021-03-11 19:30:56 +0200

    Initial Commit - Add Spring Boot Skeleton
    Auto-generated at https://start.spring.io

---
+++ cli-client/src/main/java/ntua/softeng28/evcharge/cliclient/CliClientApplication.java
@@ -0,0 +1,13 @@
+package ntua.softeng28.evcharge.cliclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliClientApplication.class, args);
+	}
+
+}
---
+++ cli-client/src/test/java/ntua/softeng28/evcharge/cliclient/CliClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ntua.softeng28.evcharge.cliclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
