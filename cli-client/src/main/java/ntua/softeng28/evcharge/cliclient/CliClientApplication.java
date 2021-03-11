99299a9bc8e1923a8cb5de9714e201e7c97bb6c1
Constantinos Pefkianakis <cpefkianakis@gmail.com>
2021-03-11 19:51:58 +0200

    Initial Command of the CLI Tool

--- cli-client/src/main/java/ntua/softeng28/evcharge/cliclient/CliClientApplication.java
+++ cli-client/src/main/java/ntua/softeng28/evcharge/cliclient/CliClientApplication.java
@@ -2,11 +2,16 @@ package ntua.softeng28.evcharge.cliclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
+
 
 @SpringBootApplication
 public class CliClientApplication {
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
+		ev_group28 cli = new ev_group28();
+		cli.main(args);
 		SpringApplication.run(CliClientApplication.class, args);
 	}
 
---
+++ cli-client/src/main/java/ntua/softeng28/evcharge/cliclient/ev_group28.java
@@ -0,0 +1,32 @@
+package ntua.softeng28.evcharge.cliclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.*;
+import java.util.concurrent.Callable;
+
+@Command(name = "ev_group28", mixinStandardHelpOptions = true,
+        version = "ev_group28 1.0", description = "CommandLine",
+        subcommands = {}
+        )
+public class ev_group28 implements Callable<Integer>{
+
+    public final String baseURL = "http://localhost:8765/evcharge/api";
+
+	@Override
+    public Integer call() throws IOException {
+        return 0;
+    }
+
+	public static void main(String[] args){
+        int exitCode = new CommandLine(new ev_group28()).execute(args);
+        System.exit(exitCode);
+	}
+
+}
